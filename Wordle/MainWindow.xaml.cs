4dbdd1e8b4cafb7e17d49357507f7d7c66a820f1
Samuel Molla <sambenmayu@gmail.com>
2025-07-07 09:56:29 -0700

    Stage 1 - Member 1: Set up App.xaml, MainWindow layout, and code-behind

--- Wordle/MainWindow.xaml.cs
+++ Wordle/MainWindow.xaml.cs
@@ -9,11 +9,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Wordle
+namespace WordleGame
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
