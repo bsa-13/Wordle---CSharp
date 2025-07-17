6ee7d6e8a10831068ee4b52122dd078b6010894a
bsa-13 <jattbhupinder13@gmail.com>
2025-07-17 13:35:08 -0700

    Stage 4 - Member 2: Implement SettingsService

---
+++ Wordle/Services/SettingsService.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using WordleGame.Models;
+
+namespace WordleGame.Services
+{
+    public class SettingsService
+    {
+        private const string SettingsFile = "user_settings.json";
+
+        public void SaveSettings(UserSettings settings)
+        {
+            var json = JsonConvert.SerializeObject(settings);
+            File.WriteAllText(SettingsFile, json);
+        }
+
+        public UserSettings LoadSettings()
+        {
+            if (!File.Exists(SettingsFile))
+                return new UserSettings();
+
+            var json = File.ReadAllText(SettingsFile);
+            return JsonConvert.DeserializeObject<UserSettings>(json);
+        }
+    }
