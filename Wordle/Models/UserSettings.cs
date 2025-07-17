ad4ea49b34fb5635456b0bc8d5ab73490a16deb6
bsa-13 <jattbhupinder13@gmail.com>
2025-07-17 12:53:42 -0700

    Stage 4 - Member 2: Define UserSettings model

---
+++ Wordle/Models/UserSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordleGame.Models
+{
+    public class UserSettings
+    {
+        public string Theme { get; set; }
+        public bool SoundEnabled { get; set; }
+    }
+
+}
