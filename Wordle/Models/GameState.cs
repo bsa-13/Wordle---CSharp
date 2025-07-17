e0644a2567b9cbdf0a4fe0e34a91381fb21adaf9
bsa-13 <jattbhupinder13@gmail.com>
2025-07-16 21:44:34 -0700

    basic Foundation stage 1

---
+++ Wordle/Models/GameState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordleGame.Models
+{
+    public class GameState
+    {
+        public int CurrentAttempt { get; set; }
+        public bool IsGameOver { get; set; }
+        public bool IsWin { get; set; }
+
+
+    }
+}
---
+++ Wordle/Models/WordleTile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordleGame.Models
+{
+   public class WordleTile
+    {
+        public char Letter { get; set; }
+        public string State { get; set; } // Correct, Present, Absent
+
+    }
+}
