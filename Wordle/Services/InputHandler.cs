e94c873b3d17075c5aa67ad550ff3b4dbd922ce9
bsa-13 <jattbhupinder13@gmail.com>
2025-07-17 12:46:09 -0700

    Stage2 - Member 2: Implement InputHandler service for keyboard events

---
+++ Wordle/Services/InputHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace WordleGame.Services
+{
+    public static class InputHandler
+    {
+        public static bool IsLetter(char c)
+        {
+            return char.IsLetter(c);
+        }
+
+        public static bool IsEnterKey(string key)
+        {
+            return key == "Enter";
+        }
+
+        public static bool IsBackspaceKey(string key)
+        {
+            return key == "Backspace";
+        }
+    }
+}
