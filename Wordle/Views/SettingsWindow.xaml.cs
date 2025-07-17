dbd2788ece7cb90dc35373a9e7da85d359e54424
bsa-13 <jattbhupinder13@gmail.com>
2025-07-17 13:37:54 -0700

    Stage 4 - Member 2: Add Settings window UI and logic

---
+++ Wordle/Views/SettingsWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using WordleGame.Models;
+using WordleGame.Services;
+
+namespace WordleGame.Views
+{
+    public partial class SettingsWindow : Window
+    {
+        private readonly SettingsService service = new SettingsService();
+
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSaveClicked(object sender, RoutedEventArgs e)
+        {
+            var settings = new UserSettings
+            {
+                SoundEnabled = SoundCheckbox.IsChecked == true,
+                Theme = ((ComboBoxItem)ThemeSelector.SelectedItem)?.Content.ToString() ?? "Light"
+            };
+
+            service.SaveSettings(settings);
+            Close();
+        }
+    }
+}
+
+
+
